c790b819e6fb7ed9515e6d2ff7f793e5ccce2f91
Chamith Siriwardena <chamith@gmail.com>
2020-07-03 13:36:27 +0000

    getting scheme data into ws

--- data/ws_dao.py
+++ data/ws_dao.py
@@ -25,8 +25,12 @@ def get_by_user_id(user_id):
     user_key = db.key('user', user_id)
     get_ws_query = db.query(kind=KIND, ancestor=user_key)
     result = list(get_ws_query.fetch())
+
     for ws in result:
+        scheme = scheme_dao.get_by_id(ws['scheme_id'])
         ws['id'] = ws.key.id
+        ws['start_date'] = scheme['start_date']
+        ws['end_date'] = scheme['end_date']
     return result 
 
 def add_all(user_id):
